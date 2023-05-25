d62264c57ece38e1611fde16ef97b088e74c8010
DDooTTaa <dimeon@naver.com>
2023-05-25 21:16:32 +0900

    리액트 프로젝트 생성

--- nogwari-client/src/App.js
+++ nogwari-client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          노과리 프로젝트
+          노과리 프로젝트 test
         </p>
       </header>
     </div>
