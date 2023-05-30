4cc1b1ce0ab51ee4c66f4e24f0e6ad4a5e314b39
DDooTTaa <dimeon@naver.com>
2023-05-30 17:09:09 +0900

    API 호출 테스트

--- nogwari-client/src/App.js
+++ nogwari-client/src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import styled from "styled-components";
 
+const BoardTest = styled.div`
+    display: flex;
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          노과리 프로젝트 배포
-        </p>
-      </header>
-    </div>
-  );
+    const [board, setBoard] = useState([]);
+    useEffect(() => {
+        const fetchData = async() => {
+            const res = await fetch('http://18.224.69.86:3000/board');
+            const result = res.json();
+            return result;
+        }
+        fetchData().then(res => setBoard(res));
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                {board && board.map(x =>
+                    <BoardTest>
+                        {x.content}
+                    </BoardTest>
+                    )
+                }
+                <p>
+                    노과리 프로젝트 배포
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
