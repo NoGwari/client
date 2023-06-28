d10f6851b10789ab6efc1468f2a5ca83c8cb9ce9
고재현 <gojh1013@gmail.com>
2023-06-28 21:41:38 +0900

    My.tsx 수정

--- nogwari-client/src/component/utill/My.tsx
+++ nogwari-client/src/component/utill/My.tsx
@@ -1,14 +1,24 @@
-import styled from "styled-components";
-import {useNavigate} from "react-router";
-import Layout from "../layout/Layout";
+import styled from 'styled-components';
+import Layout from '../layout/Layout';
+
+const MyContainer = styled.div`
+  
+`;
+
+const MyText = styled.p`
+  
+  color: darkblue;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function My(): JSX.Element {
-    return (
-        <>
-        <Layout></Layout>  
-        여기는 마이페이지 입니다.
-        </>
-    );
+  return (
+    <MyContainer>
+      <Layout />
+      <MyText>여기는 마이페이지 입니다.</MyText>
+    </MyContainer>
+  );
 }
 
 export default My;
