8d19d8f12a8255c0fb3d86461be5f473ec30b6fe
고재현 <gojh1013@gmail.com>
2023-07-26 12:16:30 +0900

    이미지 불러오기 기능

--- nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
+++ nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
@@ -1,45 +1,26 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Http } from '../../../common';
 import styled from 'styled-components';
-import { useRef } from 'react';
 import { useParams } from 'react-router';
 
-const title = styled.div``;
-const content = styled.textarea``;
+const Title = styled.div``;
+const Content = styled.textarea``;
 
 function CreateBoard() {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const categoryId = 1; //이거 드롭다운으로 바꾸기
+    const categoryId = 1; // 이거 드롭다운으로 바꾸기
+
+    const fileInputRef = useRef(null);
+    const [imageFiles, setImageFiles] = useState([]);
+
     const createNewPosting = async (title, content, categoryId) => {
-        const url = Http + '/board'; // 백엔드 서버의 엔드포인트 주소
-
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
-                },
-                body: JSON.stringify({ title, content, categoryId }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error creating new posting:', error.message);
-            throw error;
-        }
+        // ... 기존의 createNewPosting 함수 코드 ...
     };
 
     const onChangeTitle = (e) => {
         const { value } = e.target;
         setTitle(value);
-        // setInputCountTitle(e.target.value.length);
     };
 
     const onChangeContent = (e) => {
@@ -58,36 +39,61 @@ function CreateBoard() {
             });
     };
 
-    const FileUpLoad = () => {
-        const fileInputRef = useRef < HTMLInputElement > null;
-        const [imageFile, setImageFile] = (useState < UploadImage) | (null > null);
-        const handleClickFileInput = () => {
-            fileInputRef.current?.click();
-        };
-        const uploadProfile = () => {
-            const fileList = e.target.files;
-            const length = fileList?.length;
-            if (fileList && fileList[0]) {
-                const url = URL.createObjectURL(fileList[0]);
+    const handleFileChange = (e) => {
+        const fileList = e.target.files;
+        if (fileList && fileList.length > 0) {
+            const newImages = Array.from(fileList).map((file) => ({
+                file: file,
+                thumbnail: URL.createObjectURL(file),
+                type: file.type.slice(0, 5),
+            }));
 
-                setImageFile({
-                    file: fileList[0],
-                    thumbnail: url,
-                    type: fileList[0].type.slice(0, 5),
-                });
-            }
-        };
+            setImageFiles((prevImages) => [...prevImages, ...newImages]);
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input value={title} onChange={onChangeTitle} type="text" placeholder="제목" maxLength={20} />
-            <br></br>
-            <input value={content} onChange={onChangeContent} type="textarea" placeholder="내용" height="400px" />
+            <form onSubmit={handleSubmit}>
 
-            <br></br>
-            <button type="submit">게시물 생성</button>
-        </form>
+        <Title>
+            <input value={title} onChange={onChangeTitle} type="text" placeholder="제목" maxLength={20} />
+        </Title>
+        <br />
+        {imageFiles.length > 0 && (
+                    <div>
+                        {imageFiles.map((image, index) => (
+                            <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+                                <div style={{ marginRight: '10px' }}>
+                                    <img src={image.thumbnail} alt={`미리보기${index}`} style={{ width: '100px', height: 'auto' }} />
+                                </div>
+                                <div>
+                                    <p>이미지 이름: {image.file.name}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+        <br />
+        <Content
+            value={content}
+            onChange={onChangeContent}
+            placeholder="내용"
+            style={{ width: '400px', height: '400px' }}
+            maxLength={50}
+        />
+        <br />
+        <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    style={{ display: 'none' }}
+                    multiple 
+                />
+                <button type="button" onClick={() => fileInputRef.current?.click()}>
+                    이미지 선택
+                </button>
+        <button type="submit">게시물 생성</button>
+    </form>
     );
 }
 
