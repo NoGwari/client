57c692e2f3b450704277dd8035e9f4aec8e7b1a7
KKardd <wlstjd4013@gmail.com>
2023-08-17 20:05:19 +0900

    fix: add default value to select category

--- nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
+++ nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
@@ -1,19 +1,19 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
-import { Http } from '../../../common';
-import styled from 'styled-components';
-import Layout from 'component/layout/Layout';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import React, {useState, useRef, useEffect, useMemo} from "react";
+import {Http} from "../../../common";
+import styled from "styled-components";
+import Layout from "component/layout/Layout";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Title = styled.div``;
 const Content = styled.textarea``;
 
 function CreateBoard() {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
     const [categories, setCategories] = useState([]);
-    const [categoryId, setCategoryId] = useState('');
-    const [editorHtml, setEditorHtml] = useState('');
+    const [categoryId, setCategoryId] = useState("");
+    const [editorHtml, setEditorHtml] = useState("");
     const [imageFiles, setImageFiles] = useState([]);
     const editorRef = useRef(null);
     const QuillRef = useRef();
@@ -21,14 +21,14 @@ function CreateBoard() {
     useEffect(() => {
         const fetchCategories = async () => {
             try {
-                const response = await fetch(Http + '/category');
+                const response = await fetch(Http + "/category");
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error("Network response was not ok");
                 }
                 const data = await response.json();
                 setCategories(data);
             } catch (error) {
-                console.log('에러', error.message);
+                console.log("에러", error.message);
             }
         };
         fetchCategories();
@@ -40,7 +40,7 @@ function CreateBoard() {
     };
 
     const onChangeTitle = (e) => {
-        const { value } = e.target;
+        const {value} = e.target;
         setTitle(value);
     };
 
@@ -49,50 +49,50 @@ function CreateBoard() {
     };
 
     const handleImageUpload = () => {
-        const input = document.createElement('input');
+        const input = document.createElement("input");
         const formData = new FormData();
 
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
+        input.setAttribute("type", "file");
+        input.setAttribute("accept", "image/*");
         input.click();
 
         input.onchange = async () => {
             const file = input.files[0];
             if (file != null) {
-                formData.append('image', file);
+                formData.append("image", file);
 
                 if (QuillRef.current) {
-                    const url = Http + '/board/upload';
+                    const url = Http + "/board/upload";
 
                     try {
                         const response = await fetch(url, {
-                            method: 'POST',
+                            method: "POST",
                             headers: {
-                                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                                Authorization: `Bearer ${localStorage.getItem("token")}`,
                             },
                             body: formData,
                         });
 
                         if (!response.ok) {
-                            throw new Error('Network response was not ok');
+                            throw new Error("Network response was not ok");
                         }
-                        console.log('성공한듯?');
+                        console.log("성공한듯?");
                         const data = await response.json();
                         const imageUrl = data.imageUrl;
 
                         const range = QuillRef.current.getEditor().getSelection();
                         const quill = QuillRef.current.getEditor();
 
-                        quill.insertEmbed(range.index, 'image', imageUrl);
+                        quill.insertEmbed(range.index, "image", imageUrl);
                         console.log(imageUrl);
                         console.log(data);
 
                         setImageFiles((prevImageFiles) => [...prevImageFiles, file]);
                     } catch (error) {
-                        console.error('이미지 업로드 에러:', error);
+                        console.error("이미지 업로드 에러:", error);
                     }
                 } else {
-                    console.warn('QuillRef.current is not available yet');
+                    console.warn("QuillRef.current is not available yet");
                 }
             }
         };
@@ -102,18 +102,18 @@ function CreateBoard() {
         () => ({
             toolbar: {
                 container: [
-                    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-                    [{ font: [] }],
-                    [{ align: [] }],
-                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                    [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+                    [{header: [1, 2, 3, 4, 5, 6, false]}],
+                    [{font: []}],
+                    [{align: []}],
+                    ["bold", "italic", "underline", "strike", "blockquote"],
+                    [{list: "ordered"}, {list: "bullet"}, "link"],
                     [
                         {
-                            color: ['#000000', '#e60000', '#ff9900', 'custom-color'],
+                            color: ["#000000", "#e60000", "#ff9900", "custom-color"],
                         },
-                        { background: [] },
+                        {background: []},
                     ],
-                    ['image', 'video'],
+                    ["image", "video"],
                 ],
                 handlers: {
                     image: handleImageUpload,
@@ -139,39 +139,39 @@ function CreateBoard() {
     // };
 
     const createNewPosting = async (title, content, categoryId) => {
-        const url = Http + '/board';
+        const url = Http + "/board";
 
         try {
             const response = await fetch(url, {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
                 },
-                body: JSON.stringify({ title, content, categoryId }),
+                body: JSON.stringify({title, content, categoryId}),
             });
-            console.log('잘 간다');
+            console.log("잘 간다");
 
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
             }
 
             const data = await response.json();
             return data;
         } catch (error) {
-            console.log('Error creating new Posting', error.message);
+            console.log("Error creating new Posting", error.message);
             throw error;
         }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('눌림');
+        console.log("눌림");
         try {
             const newPosts = await createNewPosting(title, editorHtml, categoryId); // 이미지가 포함된 내용 전달
-            console.log('New posting created:', newPosts);
+            console.log("New posting created:", newPosts);
         } catch (error) {
-            console.error('Error creating new posting:', error.message);
+            console.error("Error creating new posting:", error.message);
         }
     };
 
@@ -183,10 +183,13 @@ function CreateBoard() {
             </Title>
             <br />
             <select value={categoryId} onChange={onChangeCategoryId}>
+                <option value="">카테고리 선택</option>
                 {categories.map((item) => (
-                    <option key={item.id} value={item.id}>
-                        {item.name}
-                    </option>
+                    <>
+                        <option key={item.id} value={item.id}>
+                            {item.name}
+                        </option>
+                    </>
                 ))}
             </select>
             <ReactQuill
