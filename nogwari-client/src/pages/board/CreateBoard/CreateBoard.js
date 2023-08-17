7a8b373c495e94d174eab5575d4965fbe95377cc
고재현 <gojh1013@gmail.com>
2023-08-17 18:00:25 +0900

    image API 전송

--- nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
+++ nogwari-client/src/pages/board/CreateBoard/CreateBoard.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { Http } from '../../../common';
 import styled from 'styled-components';
 import Layout from 'component/layout/Layout';
-import ReactQuill, { Quill } from 'react-quill';
+import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const Title = styled.div``;
@@ -16,6 +16,7 @@ function CreateBoard() {
     const [editorHtml, setEditorHtml] = useState('');
     const [imageFiles, setImageFiles] = useState([]);
     const editorRef = useRef(null);
+    const QuillRef = useRef();
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -38,127 +39,86 @@ function CreateBoard() {
         setCategoryId(selectedCategoryId);
     };
 
-    // const fileInputRef = useRef(null);
-    // const [imageFiles, setImageFiles] = useState([]);
-
     const onChangeTitle = (e) => {
         const { value } = e.target;
         setTitle(value);
     };
 
-    // const onChangeContent = (e) => {
-    //     const { value } = e.target;
-    //     setContent(value);
-    // };
-
-    // const handleFileChange = (e) => {
-    //     const fileList = e.target.files;
-    //     if (fileList && fileList.length > 0) {
-    //         const newImages = Array.from(fileList).map((file) => ({
-    //             file: file,
-    //             thumbnail: URL.createObjectURL(file),
-    //             type: file.type.slice(0, 5),
-    //         }));
-
-    //         setImageFiles((prevImages) => [...prevImages, ...newImages]);
-    //     }
-    // };
-
-    // const handleImageDelete = (index) => {
-    //     setImageFiles((prevImages) => {
-    //         const newImages = [...prevImages];
-    //         newImages.splice(index, 1); // 해당 인덱스의 이미지 제거
-    //         return newImages;
-    //     });
-    // };
-
-    const modules = {
-        toolbar: {
-            container: [
-                [{ header: [1, 2, 3, 4, 5, 6, false] }],
-                [{ font: [] }],
-                [{ align: [] }],
-                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
-                [
-                    {
-                        color: [
-                            '#000000',
-                            '#e60000',
-                            '#ff9900',
-                            '#ffff00',
-                            '#008a00',
-                            '#0066cc',
-                            '#9933ff',
-                            '#ffffff',
-                            '#facccc',
-                            '#ffebcc',
-                            '#ffffcc',
-                            '#cce8cc',
-                            '#cce0f5',
-                            '#ebd6ff',
-                            '#bbbbbb',
-                            '#f06666',
-                            '#ffc266',
-                            '#ffff66',
-                            '#66b966',
-                            '#66a3e0',
-                            '#c285ff',
-                            '#888888',
-                            '#a10000',
-                            '#b26b00',
-                            '#b2b200',
-                            '#006100',
-                            '#0047b2',
-                            '#6b24b2',
-                            '#444444',
-                            '#5c0000',
-                            '#663d00',
-                            '#666600',
-                            '#003700',
-                            '#002966',
-                            '#3d1466',
-                            'custom-color',
-                        ],
-                    },
-                    { background: [] },
-                ],
-                ['image', 'video'],
-                ['clean'],
-            ],
-        },
-    };
-
     const handleEditorChange = (html) => {
         setEditorHtml(html);
     };
-    const handleImageUpload = (file) => {
-        console.log('이미지이미지이미지이ㅣ지');
+
+    const handleImageUpload = () => {
+        const input = document.createElement('input');
         const formData = new FormData();
-        formData.append('image', file);
 
-        const url = Http + '/board/upload';
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        input.click();
 
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-            body: formData,
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const imageUrl = data.imageUrl;
+        input.onchange = async () => {
+            const file = input.files[0];
+            if (file != null) {
+                formData.append('image', file);
 
-                const range = editorRef.getEditor().getSelection();
-                editorRef.getEditor().insertEmbed(range.index, 'image', imageUrl);
+                if (QuillRef.current) {
+                    const url = Http + '/board/upload';
 
-                setImageFiles((prevImageFiles) => [...prevImageFiles, file]);
-            })
-            .catch((error) => {
-                console.error('이미지 업로드 에러:', error);
-            });
+                    try {
+                        const response = await fetch(url, {
+                            method: 'POST',
+                            headers: {
+                                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                            },
+                            body: formData,
+                        });
+                        imageUrl = response.data.url;
+
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+
+                        const data = await response.json();
+                        const imageUrl = data.imageUrl;
+
+                        const range = QuillRef.current.getEditor().getSelection();
+                        QuillRef.current.getEditor().insertEmbed(range.index, 'image', imageUrl);
+
+                        setImageFiles((prevImageFiles) => [...prevImageFiles, file]);
+                    } catch (error) {
+                        console.error('이미지 업로드 에러:', error);
+                    }
+                } else {
+                    console.warn('QuillRef.current is not available yet');
+                }
+            }
+        };
     };
+
+    const modules = useMemo(
+        () => ({
+            toolbar: {
+                container: [
+                    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                    [{ font: [] }],
+                    [{ align: [] }],
+                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                    [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+                    [
+                        {
+                            color: ['#000000', '#e60000', '#ff9900', 'custom-color'],
+                        },
+                        { background: [] },
+                    ],
+                    ['image', 'video'],
+                ],
+                handlers: {
+                    image: handleImageUpload,
+                },
+            },
+        }),
+        []
+    );
 
     const handleDrop = (e) => {
         e.preventDefault();
@@ -234,8 +194,11 @@ function CreateBoard() {
                 onChange={handleEditorChange}
                 modules={modules}
                 theme="snow"
-                ref={editorRef}
-                onPaste={(e) => handleImageUpload(e.clipboardData.files[0])}
+                ref={(element) => {
+                    if (element != null) {
+                        QuillRef.current = element;
+                    }
+                }}
             />
             <button onClick={handleSubmit}>게시물 작성</button>
         </div>
