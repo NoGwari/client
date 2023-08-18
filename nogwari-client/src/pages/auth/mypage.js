034ccbf167d832374cc68fdb0c476e29f6ece1fa
고재현 <gojh1013@gmail.com>
2023-08-18 17:23:01 +0900

    닉네임변경 기능 구현중

--- nogwari-client/src/pages/auth/mypage.js
+++ nogwari-client/src/pages/auth/mypage.js
@@ -4,6 +4,8 @@ import Layout from 'component/layout/Layout';
 
 function Mypage() {
     const [me, setMe] = useState(null);
+    const [newNickname, setNewNickname] = useState('');
+    const [isUpdatingNickname, setIsUpdatingNickname] = useState(false);
 
     useEffect(() => {
         const fetchMyPage = async () => {
@@ -26,6 +28,32 @@ function Mypage() {
         };
         fetchMyPage();
     }, []);
+    const handleNicknameChange = async () => {
+        if (!newNickname) {
+            return;
+        }
+        setIsUpdatingNickname(true);
+
+        try {
+            const response = await fetch(Http + '/auth/updatenick', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+                body: JSON.stringify({ nickname: newNickname }),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('변경오류', error.message);
+        } finally {
+            setIsUpdatingNickname(false);
+        }
+    };
 
     return (
         <>
@@ -37,6 +65,14 @@ function Mypage() {
                         <p>사용자 닉네임: {me.nickname}</p>
                         <p>이메일: {me.email}</p>
                         <p>사용자 이미지: {me.img}</p>
+                        <br />
+                        <div>
+                            <p>닉네임 변경:</p>
+                            <input type="text" value={newNickname} onChange={(e) => setNewNickname(e.target.value)} />
+                            <button onClick={handleNicknameChange} disabled={isUpdatingNickname}>
+                                {isUpdatingNickname ? '변경 중...' : '변경'}
+                            </button>
+                        </div>
                     </div>
                 ) : (
                     <p>로그인 정보를 불러오는 중...</p>
