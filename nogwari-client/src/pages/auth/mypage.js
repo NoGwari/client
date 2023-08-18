5fbbd558d824b57e0dc8906e14ddb297a23b9270
KKardd <wlstjd4013@gmail.com>
2023-08-18 19:01:33 +0900

    fix: backend router path

--- nogwari-client/src/pages/auth/mypage.js
+++ nogwari-client/src/pages/auth/mypage.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Http } from 'common';
-import Layout from 'component/layout/Layout';
+import React, {useEffect, useState} from "react";
+import {Http} from "common";
+import Layout from "component/layout/Layout";
 
 function Mypage() {
     const [me, setMe] = useState(null);
-    const [newNickname, setNewNickname] = useState('');
+    const [newNickname, setNewNickname] = useState("");
     const [isUpdatingNickname, setIsUpdatingNickname] = useState(false);
 
     useEffect(() => {
         const fetchMyPage = async () => {
             try {
-                const response = await fetch(Http + '/auth/me', {
+                const response = await fetch(Http + "/auth/me", {
                     headers: {
-                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
                     },
                 });
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error("Network response was not ok");
                 }
 
                 const data = await response.json();
                 setMe(data);
                 console.log(data);
             } catch (error) {
-                console.log('에러', error.message);
+                console.log("에러", error.message);
             }
         };
         fetchMyPage();
@@ -35,21 +35,22 @@ function Mypage() {
         setIsUpdatingNickname(true);
 
         try {
-            const response = await fetch(Http + '/auth/updatenick', {
-                method: 'PUT',
+            const response = await fetch(Http + "/user/updatenick", {
+                method: "PUT",
                 headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
                 },
-                body: JSON.stringify({ nickname: newNickname }),
+                body: JSON.stringify({nickname: newNickname}),
             });
             if (!response.ok) {
-                throw new Error('Network response was not ok.');
+                throw new Error("Network response was not ok.");
             }
+            console.log("res:", response);
             const data = await response.json();
             console.log(data);
         } catch (error) {
-            console.error('변경오류', error.message);
+            console.error("변경오류", error.message);
         } finally {
             setIsUpdatingNickname(false);
         }
@@ -70,7 +71,7 @@ function Mypage() {
                             <p>닉네임 변경:</p>
                             <input type="text" value={newNickname} onChange={(e) => setNewNickname(e.target.value)} />
                             <button onClick={handleNicknameChange} disabled={isUpdatingNickname}>
-                                {isUpdatingNickname ? '변경 중...' : '변경'}
+                                {isUpdatingNickname ? "변경 중..." : "변경"}
                             </button>
                         </div>
                     </div>
