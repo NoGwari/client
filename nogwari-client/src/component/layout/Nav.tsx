c166492ea6ff3d4de83c5e426b9993430a3f5b5f
unknown <gojh1013@gmail.com>
2023-08-27 18:51:41 +0900

    category 상단에 띄우기

--- nogwari-client/src/component/layout/Nav.tsx
+++ nogwari-client/src/component/layout/Nav.tsx
@@ -1,45 +1,59 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import styled from "styled-components";
-import Navbar from "component/layout/Navbar";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Http } from 'common';
 
-const NavContainer = styled.div`
-  margin-top: 70px;
-  display: flex;
-  width: 100%;
-  height: 100px;
-  background-color: whitesmoke;
-`
-const GirdBpx = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 1fr 2fr);
-`
-const LinkContainer = styled.div`
-border: 1px solid black;
-`
-
-const LinkBox: React.FC = () => {
-    return (
-        <>
-        <LinkContainer>
-            <Link to={"/bc"}>
-                카테고리1
-            </Link>
-        </LinkContainer>
-        </>
-    )
+interface Category {
+    id: number;
+    name: string;
+    post_num: number;
 }
 
+const NavContainer = styled.div`
+    margin-top: 70px;
+    display: flex;
+    width: 100%;
+    height: 100px;
+    background-color: whitesmoke;
+`;
+
+const GridBox = styled.div`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 16px;
+`;
+
 const Nav: React.FC = () => {
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(Http + '/category');
+                if (!response.ok) {
+                    throw new Error('API 요청 실패');
+                }
+                const data = (await response.json()) as Category[];
+                setCategories(data);
+            } catch (error) {
+                console.error('카테고리 가져오는 중 에러발생: ', error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
-        <NavContainer>
-                <GirdBpx>
-                    <Link to={"/bc"}>
-                        카테고리1
-                    </Link>
-                </GirdBpx>
-        </NavContainer>
+            <NavContainer>
+                <GridBox>
+                    {categories.map((category, index) => (
+                        // 각 링크 안에 카테고리 이름만 표시합니다.
+                        <Link key={index} to={`/board/${category.name}`}>
+                            {category.name}
+                        </Link>
+                    ))}
+                </GridBox>
+            </NavContainer>
         </>
     );
 };
