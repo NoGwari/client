d2441f070b8ea3ddccb6edacaa273d8558e03afa
고재현 <gojh1013@gmail.com>
2023-09-02 16:13:50 +0900

    사용자 이미지 업로드

--- nogwari-client/src/pages/auth/mypage.js
+++ nogwari-client/src/pages/auth/mypage.js
@@ -1,31 +1,68 @@
-import React, {useEffect, useState} from "react";
-import {Http} from "common";
-import Layout from "component/layout/Layout";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Http } from 'common';
+import Layout from 'component/layout/Layout';
+import { useNavigate } from 'react-router-dom';
 
 function Mypage() {
     const [me, setMe] = useState(null);
-    const [newNickname, setNewNickname] = useState("");
+    const [newNickname, setNewNickname] = useState('');
     const [isUpdatingNickname, setIsUpdatingNickname] = useState(false);
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const [imageFile, setImageFile] = useState(null);
+    const [isUploadingImage, setIsUploadingImage] = useState(false);
+
+    const handleImageChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setImageFile(selectedFile);
+    };
+
+    const handleImageUpload = async () => {
+        if (!imageFile) {
+            return;
+        }
+        setIsUploadingImage(true);
+
+        try {
+            const formData = new FormData();
+            formData.append('image', imageFile);
+
+            const response = await fetch(Http + '/auth/me', {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+                body: formData,
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+
+            console.log('이미지 업로드 완료');
+        } catch (error) {
+            console.error('이미지 업로드 오류', error.message);
+        } finally {
+            setIsUploadingImage(false);
+        }
+    };
 
     useEffect(() => {
         const fetchMyPage = async () => {
             try {
-                const response = await fetch(Http + "/auth/me", {
+                const response = await fetch(Http + '/auth/me', {
                     headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
                     },
                 });
                 if (!response.ok) {
-                    throw new Error("Network response was not ok");
+                    throw new Error('Network response was not ok');
                 }
 
                 const data = await response.json();
                 setMe(data);
                 console.log(data);
             } catch (error) {
-                console.log("에러", error.message);
+                console.log('에러', error.message);
             }
         };
         fetchMyPage();
@@ -37,30 +74,30 @@ function Mypage() {
         setIsUpdatingNickname(true);
 
         try {
-            const response = await fetch(Http + "/user/updatenick", {
-                method: "PUT",
+            const response = await fetch(Http + '/user/updatenick', {
+                method: 'PUT',
                 headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
                 },
-                body: JSON.stringify({nickname: newNickname}),
+                body: JSON.stringify({ nickname: newNickname }),
             });
             if (!response.ok) {
-                throw new Error("Network response was not ok.");
+                throw new Error('Network response was not ok.');
             }
-            console.log("res:", response);
+            console.log('res:', response);
             const data = await response.json();
             console.log(data);
         } catch (error) {
-            console.error("변경오류", error.message);
+            console.error('변경오류', error.message);
         } finally {
             setIsUpdatingNickname(false);
         }
     };
-    const logOut= ()=>{
-        localStorage.removeItem("token");
-        navigate("/")
-    }
+    const logOut = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
 
     return (
         <>
@@ -72,12 +109,19 @@ function Mypage() {
                         <p>사용자 닉네임: {me.nickname}</p>
                         <p>이메일: {me.email}</p>
                         <p>사용자 이미지: {me.img}</p>
+                        <div>
+                            <p>이미지 변경:</p>
+                            <input type="file" accept="image/*" onChange={handleImageChange} />
+                            <button onClick={handleImageUpload} disabled={isUploadingImage}>
+                                {isUploadingImage ? '업로드 중...' : '업로드'}
+                            </button>
+                        </div>
                         <br />
                         <div>
                             <p>닉네임 변경:</p>
                             <input type="text" value={newNickname} onChange={(e) => setNewNickname(e.target.value)} />
                             <button onClick={handleNicknameChange} disabled={isUpdatingNickname}>
-                                {isUpdatingNickname ? "변경 중..." : "변경"}
+                                {isUpdatingNickname ? '변경 중...' : '변경'}
                             </button>
                         </div>
                         <button onClick={logOut}>로그아웃</button>
