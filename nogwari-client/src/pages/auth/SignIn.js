6e4db7c25eb8b60b67c0676ebd9dfab7d962bbed
unknown <gojh1013@gmail.com>
2023-10-26 20:01:56 +0900

    Test : password RegExp

--- nogwari-client/src/pages/auth/SignIn.js
+++ nogwari-client/src/pages/auth/SignIn.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Http } from '../../common';
 import styled from 'styled-components';
 import Layout from 'component/layout/Layout';
+import { useCallback } from 'react';
 
 const SignInContainer = styled.div`
     text-align: center;
@@ -52,6 +53,7 @@ const VerifyButton = styled.button`
     border: none;
     cursor: pointer;
 `;
+const TextField = styled.input``;
 
 function SignIn() {
     const [email, setEmail] = useState('');
@@ -60,6 +62,12 @@ function SignIn() {
     const [passwordCheck, setPasswordCheck] = useState('');
     const [nickname, setNickname] = useState('');
     const [showVerifyNum, setShowVerifyNum] = useState(false);
+    const [passwordMessage, setPasswordMessage] = useState('');
+    const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
+
+    const [isName, setIsName] = useState(false);
+    const [isPassword, setIsPassword] = useState(false);
+    const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
 
     const onChangeEmail = (e) => {
         const { value } = e.target;
@@ -70,14 +78,36 @@ function SignIn() {
         const { value } = e.target;
         setVerifyKey(value);
     };
-    const onChangePassword = (e) => {
-        const { value } = e.target;
-        setPassword(value);
-    };
-    const onChangePasswordCheck = (e) => {
-        const { value } = e.target;
-        setPasswordCheck(value);
-    };
+    const onChangePassword = useCallback((e) => {
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+        const passwordCurrent = e.target.value;
+        setPassword(passwordCurrent);
+
+        if (!passwordRegex.test(passwordCurrent)) {
+            setPasswordMessage('숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!');
+            setIsPassword(false);
+        } else {
+            setPasswordMessage('안전한 비밀번호에요 : )');
+            setIsPassword(true);
+        }
+    }, []);
+
+    const onChangePasswordCheck = useCallback(
+        (e) => {
+            const passwordConfirmCurrent = e.target.value;
+            setPasswordCheck(passwordConfirmCurrent);
+
+            if (password === passwordConfirmCurrent) {
+                setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 :)');
+                setIsPasswordConfirm(true);
+            } else {
+                setPasswordConfirmMessage('비밀번호가 틀려요. 다시 확인해주세요 :(');
+                setIsPasswordConfirm(false);
+            }
+        },
+        [password]
+    );
+
     const onChangeNickname = (e) => {
         const { value } = e.target;
         setNickname(value);
@@ -181,6 +211,35 @@ function SignIn() {
                     ) : (
                         <Button type="submit">이메일 인증</Button>
                     )}
+                </SignInForm>
+            </SignInContainer>
+            <SignInContainer>
+                <SignInword>정보등록</SignInword>
+                <SignInForm>
+                    <div className="formbox">
+                        <Input
+                            style={{ fontSize: '14px' }}
+                            value={password}
+                            onChange={onChangePassword}
+                            type="password"
+                            placeholder="영어+숫자+특수기호 포함 8자리이상"
+                            maxLength={20}
+                        />
+                        <span className={`message ${checkPasswordMatch ? 'success' : 'error'}`}>{passwordMessage}</span>
+                    </div>
+                    <div className="formbox">
+                        <Input
+                            style={{ fontSize: '14px' }}
+                            value={passwordCheck}
+                            onChange={onChangePasswordCheck}
+                            type="password"
+                            placeholder="영어+숫자+특수기호 포함 8자리이상"
+                            maxLength={20}
+                        />
+                        <span className={`message ${isPasswordConfirm ? 'success' : 'error'}`}>
+                            {passwordConfirmMessage}
+                        </span>
+                    </div>
                 </SignInForm>
             </SignInContainer>
         </>
