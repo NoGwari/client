d6b2cf84ce92c927683afbbe14be0782805aa797
고재현 <gojh1013@gmail.com>
2023-11-14 14:28:55 +0900

    Feat : show temporary password

--- nogwari-client/src/pages/auth/ForgotPassword.js
+++ nogwari-client/src/pages/auth/ForgotPassword.js
@@ -30,34 +30,6 @@ const Input = styled.input`
     margin-bottom: 10px;
     box-sizing: border-box;
 `;
-
-const Button = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-`;
-const SignInContainer = styled.div`
-    text-align: center;
-`;
-const SignInword = styled.div`
-    color: black;
-    font-weight: bold;
-    font-size: 20px;
-    text-align: center;
-    margin-top: 80px;
-`;
-const SignInForm = styled.form`
-    item-align: center;
-    max-width: 300px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    background-color: #f9f9f9;
-    margin-top: 20px;
-`;
 const StyledButton = styled.button`
     width: 100%;
     padding: 10px;
@@ -102,15 +74,24 @@ const FormBox = styled.div`
     }
 `;
 
+const TemporaryPassword = styled.div`
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+`;
+
 function ForgotPassword() {
+    const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [verifyKey, setVerifyKey] = useState('');
+    const [randomPassword, setRandomPassword] = useState('');
     const [showVerifyNum, setShowVerifyNum] = useState(false);
 
     const [EmailMessage, setEmailMessage] = useState('');
 
     const [isEmail, setIsEmail] = useState(false);
-    const [ischeckKey, setIscheckKey] = useState(false);
+    const [isVerifyNumEntered, setIsVerifyNumEntered] = useState(false);
 
     const onChangeEmail = useCallback((e) => {
         const EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -172,8 +153,9 @@ function ForgotPassword() {
                 const responseData = await response.json();
                 const randomPassword = responseData.randomPassword;
                 console.log('랜덤 비빌번호 :', randomPassword);
+                setRandomPassword(randomPassword);
+                setIsVerifyNumEntered(true);
             } else if (response.status === 404) {
-                setIscheckKey(false);
                 console.log('코드 불일치');
                 window.confirm('인증코드를 제대로 입력해주세요.');
             } else {
@@ -184,52 +166,70 @@ function ForgotPassword() {
             console.error('Error', error.message);
         }
     };
+
+    const gotologin = (e) => {
+        navigate('/login');
+    };
     return (
         <>
             <Layout />
-            <ForgotContainer>
-                <Forgotword>비밀번호찾기</Forgotword>
-                <ForgotForm onSubmit={ForgotPassword}>
-                    <FormBox>
-                        {showVerifyNum ? (
-                            <Input
-                                style={{ fontSize: '14px' }}
-                                show={showVerifyNum}
-                                value={verifyKey}
-                                onChange={onChangeVerifykey}
-                                type="text"
-                                placeholder="인증번호"
-                            />
-                        ) : (
-                            <div>
+            {isVerifyNumEntered ? (
+                <ForgotContainer>
+                    <Forgotword>임시 비밀번호</Forgotword>
+                    <ForgotForm>
+                        <FormBox>
+                            <TemporaryPassword>{randomPassword}</TemporaryPassword>
+                        </FormBox>
+                        <FormBox>
+                            <VerifyButton onClick={gotologin}>로그인하러가기</VerifyButton>
+                        </FormBox>
+                    </ForgotForm>
+                </ForgotContainer>
+            ) : (
+                <ForgotContainer>
+                    <Forgotword>비밀번호찾기</Forgotword>
+                    <ForgotForm onSubmit={ForgotPassword}>
+                        <FormBox>
+                            {showVerifyNum ? (
                                 <Input
-                                    value={email}
-                                    onChange={onChangeEmail}
+                                    style={{ fontSize: '14px' }}
+                                    show={showVerifyNum}
+                                    value={verifyKey}
+                                    onChange={onChangeVerifykey}
                                     type="text"
-                                    placeholder="E-mail"
-                                    maxLength={40}
+                                    placeholder="인증번호"
                                 />
-                                <span
-                                    style={{ fontSize: '13px' }}
-                                    className={`message ${isEmail ? 'success' : 'error'}`}
-                                >
-                                    {EmailMessage}
-                                </span>
-                            </div>
-                        )}
-                    </FormBox>
-                    <FormBox>
-                        {showVerifyNum ? (
-                            <VerifyButton onClick={InitPassword}>인증코드 입력</VerifyButton>
-                        ) : (
-                            <StyledButton type="submit" disabled={!isEmail}>
-                                이메일 인증
-                            </StyledButton>
-                        )}
-                    </FormBox>
-                    &nbsp;
-                </ForgotForm>
-            </ForgotContainer>
+                            ) : (
+                                <div>
+                                    <Input
+                                        value={email}
+                                        onChange={onChangeEmail}
+                                        type="text"
+                                        placeholder="E-mail"
+                                        maxLength={40}
+                                    />
+                                    <span
+                                        style={{ fontSize: '13px' }}
+                                        className={`message ${isEmail ? 'success' : 'error'}`}
+                                    >
+                                        {EmailMessage}
+                                    </span>
+                                </div>
+                            )}
+                        </FormBox>
+                        <FormBox>
+                            {showVerifyNum ? (
+                                <VerifyButton onClick={InitPassword}>인증코드 입력</VerifyButton>
+                            ) : (
+                                <StyledButton type="submit" disabled={!isEmail}>
+                                    이메일 인증
+                                </StyledButton>
+                            )}
+                        </FormBox>
+                        &nbsp;
+                    </ForgotForm>
+                </ForgotContainer>
+            )}
         </>
     );
 }
